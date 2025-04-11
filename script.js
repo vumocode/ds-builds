ea86dcf5e5a2fa9205de1bc6253d91fa15e6a353
vumocode <91375898+vumocode@users.noreply.github.com>
2025-04-11 22:43:52 +0200

    si

---
+++ script.js
@@ -0,0 +1,31 @@
+const updateDualImages = () => {
+    document.querySelectorAll("td").forEach(td => {
+      const imgs = td.querySelectorAll("img");
+      if (imgs.length === 2) {
+        if (!td.classList.contains("dual-img")) {
+          td.classList.add("dual-img");
+        }
+      } else {
+        td.classList.remove("dual-img");
+      }
+    });
+  };
+
+  // Wykonaj raz na start
+  updateDualImages();
+
+  // Obserwuj zmiany w całym dokumencie
+  const observer = new MutationObserver(mutations => {
+    for (const mutation of mutations) {
+      if (mutation.type === "childList" || mutation.type === "attributes") {
+        updateDualImages();
+        break;
+      }
+    }
+  });
+
+  observer.observe(document.body, {
+    childList: true,
+    attributes: true,
+    subtree: true
+  });
