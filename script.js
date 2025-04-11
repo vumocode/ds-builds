ce6210051ded824965c0d69b6bb2642d58e704fa
vumocode <91375898+vumocode@users.noreply.github.com>
2025-04-12 01:49:22 +0200

    as

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
 const updateDualImages = () => {
-    document.querySelectorAll("td").forEach(td => {
-      const imgs = td.querySelectorAll("img");
-      if (imgs.length === 2) {
-        if (!td.classList.contains("dual-img")) {
-          td.classList.add("dual-img");
-        }
-      } else {
-        td.classList.remove("dual-img");
+  document.querySelectorAll("td").forEach((td) => {
+    const imgs = td.querySelectorAll("img");
+    if (imgs.length === 2) {
+      if (!td.classList.contains("dual-img")) {
+        td.classList.add("dual-img");
       }
-    });
-  };
-
-  // Wykonaj raz na start
-  updateDualImages();
-
-  // Obserwuj zmiany w całym dokumencie
-  const observer = new MutationObserver(mutations => {
-    for (const mutation of mutations) {
-      if (mutation.type === "childList" || mutation.type === "attributes") {
-        updateDualImages();
-        break;
-      }
+    } else {
+      td.classList.remove("dual-img");
     }
   });
+};
 
-  observer.observe(document.body, {
-    childList: true,
-    attributes: true,
-    subtree: true
-  });
+updateDualImages();
+
+const observer = new MutationObserver((mutations) => {
+  for (const mutation of mutations) {
+    if (mutation.type === "childList" || mutation.type === "attributes") {
+      updateDualImages();
+      break;
+    }
+  }
+});
+
+observer.observe(document.body, {
+  childList: true,
+  attributes: true,
+  subtree: true,
+});
